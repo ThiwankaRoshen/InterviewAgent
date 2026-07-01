48ab2e0e9c84530d8e6ebb6adeb9fe305bcad788
ThiwankaRoshen <thiwanka.r.pathirana@gmail.com>
2026-07-01 13:04:23 +0530

    add settings for env

--- server/settings.py
+++ server/settings.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from pydantic import SecretStr
 
 class Settings(BaseSettings):
     ASSEMBLYAI_API_KEY: str
@@ -10,6 +10,10 @@ class Settings(BaseSettings):
     LANGSMITH_API_KEY: str
     LANGSMITH_PROJECT: str
     OTEL_EXPORTER_OTLP_ENDPOINT: str
+    SECRET_KEY: SecretStr
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    MAX_UPLOAD_SIZE_BYTES: int = 5 * 1024 * 1024
 
     model_config = SettingsConfigDict(
         env_file=".env",
